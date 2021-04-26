5ebc5270a7d972f33395a90407d273b21fb9271a
Igor Shevchenko <bnzone@gmail.com>
2021-04-25 22:40:00 -0400

    add seed data

---
+++ src/seed.js
@@ -0,0 +1,74 @@
+/* eslint-disable no-plusplus */
+// NOTE: replace 'NvPY9M9MzFTARQ6M816YAzDJxZ72' with your Firebase auth user id (can be taken from Firebase)
+export function seedDatabase(firebase) {
+  const users = [
+    {
+      userId: 'NvPY9M9MzFTARQ6M816YAzDJxZ72',
+      username: 'jim',
+      fullName: 'Jim Larry',
+      emailAddress: 'jimlarry@gmail.com',
+      following: ['2'],
+      followers: ['2', '3', '4'],
+      dateCreated: Date.now()
+    },
+    {
+      userId: '2',
+      username: 'silvester',
+      fullName: 'Silvester Stalone',
+      emailAddress: 'stalone@yahoo.com',
+      following: [],
+      followers: ['NvPY9M9MzFTARQ6M816YAzDJxZ72'],
+      dateCreated: Date.now()
+    },
+    {
+      userId: '3',
+      username: 'baskov',
+      fullName: 'Nikolai Baskov',
+      emailAddress: 'baskov@mail.ru',
+      following: [],
+      followers: ['NvPY9M9MzFTARQ6M816YAzDJxZ72'],
+      dateCreated: Date.now()
+    },
+    {
+      userId: '4',
+      username: 'oleg',
+      fullName: 'Oleg Smith',
+      emailAddress: 'smith@orwell.com',
+      following: [],
+      followers: ['NvPY9M9MzFTARQ6M816YAzDJxZ72'],
+      dateCreated: Date.now()
+    }
+  ];
+
+  // eslint-disable-next-line prefer-const
+  for (let k = 0; k < users.length; k++) {
+    firebase.firestore().collection('users').add(users[k]);
+  }
+
+  // eslint-disable-next-line prefer-const
+  for (let i = 1; i <= 5; ++i) {
+    firebase
+      .firestore()
+      .collection('photos')
+      .add({
+        photoId: i,
+        userId: '2',
+        imageSrc: `/images/users/raphael/${i}.jpg`,
+        caption: 'Saint George and the Dragon',
+        likes: [],
+        comments: [
+          {
+            displayName: 'baskov',
+            comment: 'Love it!'
+          },
+          {
+            displayName: 'oleg',
+            comment: 'Great Post!'
+          }
+        ],
+        userLatitude: '40.7128°',
+        userLongitude: '74.0060°',
+        dateCreated: Date.now()
+      });
+  }
+}
