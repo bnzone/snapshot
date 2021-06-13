fad178247dd6f054206e2273d7e0ed96e973bf64
Igor Shevchenko <bnzone@gmail.com>
2021-06-13 08:20:02 -0700

    add actions

--- src/components/post/header.js
+++ src/components/post/header.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default function Header({ username }) {
   return (
--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,14 +1,23 @@
 import { useRef } from 'react';
 import PropTypes from 'prop-types';
-import usePhotos from '../../hooks/usePhotos';
 import Header from './header';
 import Image from './image';
+import Actions from './actions';
 
 export default function Post({ content }) {
+  const commentInput = useRef(null);
+  const handleFocus = () => commentInput.current.focus();
+
   return (
     <div className="rounded col-span-4 border bg-white border-gray-primary mb-12">
       <Header username={content.username} />
       <Image src={content.imageSrc} caption={content.caption} />
+      <Actions
+        docId={content.docId}
+        totalLikes={content.likes.length}
+        likedPhoto={content.userLikedPhoto}
+        handleFocus={handleFocus}
+      />
     </div>
   );
 }
@@ -20,8 +29,8 @@ Post.propTypes = {
     caption: PropTypes.string.isRequired,
     docId: PropTypes.string.isRequired,
     userLikedPhoto: PropTypes.bool.isRequired,
-    comments: PropTypes.array.isRequired,
     likes: PropTypes.array.isRequired,
+    comments: PropTypes.array.isRequired,
     dateCreated: PropTypes.number.isRequired
   })
 };
