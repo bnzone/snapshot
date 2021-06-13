bc1de712dfdb2ceaa003aad213b669eb1fe21674
Igor Shevchenko <bnzone@gmail.com>
2021-06-13 08:56:43 -0700

    add initial comment component

---
+++ src/components/post/comments.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { formatDistance } from 'date-fns';
+import { Link } from 'react-router-dom';
+
+export default function Comments({
+  docId,
+  comments: allComments,
+  posted,
+  commentInput
+}) {
+  const [comments, setComments] = useState(allComments);
+
+  return (
+    <>
+      <p>Im comment</p>
+    </>
+  );
+}
+
+Comments.propTypes = {
+  docId: PropTypes.string.isRequired,
+  comments: PropTypes.array.isRequired,
+  posted: PropTypes.number.isRequired,
+  commentInput: PropTypes.object.isRequired
+};
