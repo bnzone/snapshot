b7f68081e513d763fd400e6db98875074e816598
Igor Shevchenko <bnzone@gmail.com>
2021-06-14 19:04:54 -0700

    add full functioning comment feature

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { formatDistance } from 'date-fns';
 import { Link } from 'react-router-dom';
+import AddComment from './add-comment';
 
 export default function Comments({
   docId,
@@ -10,10 +11,47 @@ export default function Comments({
   commentInput
 }) {
   const [comments, setComments] = useState(allComments);
+  const [commentsSlice, setCommentsSlice] = useState(3);
+
+  const showNextComments = () => {
+    setCommentsSlice(commentsSlice + 3);
+  };
 
   return (
     <>
-      <p>Im comment</p>
+      <div className="p-4 pt-1 pb-4">
+        {comments.slice(0, commentsSlice).map((item) => (
+          <p key={`${item.comment}-${item.displayName}`} className="mb-1">
+            <Link to={`/p/${item.displayName}`}>
+              <span className="mr-1 font-bold">{item.displayName}</span>
+            </Link>
+            <span>{item.comment}</span>
+          </p>
+        ))}
+        {comments.length >= 3 && commentsSlice < comments.length && (
+          <button
+            className="text-sm text-gray-base mb-1 cursor-pointer focus:outline-none"
+            type="button"
+            onClick={showNextComments}
+            onKeyDown={(event) => {
+              if (event.key === 'Enter') {
+                showNextComments();
+              }
+            }}
+          >
+            View more comments
+          </button>
+        )}
+        <p className="text-gray-base uppercase text-xs mt-2">
+          {formatDistance(posted, new Date())} ago
+        </p>
+      </div>
+      <AddComment
+        docId={docId}
+        comments={comments}
+        setComments={setComments}
+        commentInput={commentInput}
+      />
     </>
   );
 }
