b0f6187de8137884e1428e13a1a1c91e07090072
Igor Shevchenko <bnzone@gmail.com>
2021-06-20 16:43:07 -0700

    edit test

--- src/__tests__/pages/not-found.test.js
+++ src/__tests__/pages/not-found.test.js
@@ -1,20 +1,50 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import NotFound from '../../pages/not-found';
 import FirebaseContext from '../../context/firebase';
 import UserContext from '../../context/user';
-import NotFound from '../../pages/not-found';
-import { BrowserRouter as Router } from 'react-router-dom';
+
+const firebase = {
+  auth: jest.fn(() => ({
+    createUserWithEmailAndPassword: jest.fn(() =>
+      Promise.resolve({
+        user: {
+          updateProfile: jest.fn(() => Promise.resolve('I am signed up!'))
+        }
+      })
+    )
+  }))
+};
 
 describe('<NotFound />', () => {
-  it('renders the not found page with logged in user', () => {
+  it('renders the not found page with a logged in user', async () => {
     const { getByText } = render(
       <Router>
-        <FirebaseContext.Provider value={{}}>
-          <UserContext.Provider value={{ user: { uid: 1 } }}>
+        <FirebaseContext.Provider value={{ firebase }}>
+          <UserContext.Provider value={{ user: {} }}>
             <NotFound />
           </UserContext.Provider>
         </FirebaseContext.Provider>
       </Router>
     );
+
+    expect(getByText('Not Found!')).toBeTruthy();
+    expect(document.title).toEqual('Not Found - SnapShot');
+  });
+
+  it('renders the not found page with no active logged in user', async () => {
+    const { getByText } = render(
+      <Router>
+        <FirebaseContext.Provider value={{ firebase }}>
+          <UserContext.Provider value={{ user: null }}>
+            <NotFound />
+          </UserContext.Provider>
+        </FirebaseContext.Provider>
+      </Router>
+    );
+
+    expect(getByText('Not Found!')).toBeTruthy();
+    expect(document.title).toEqual('Not Found - SnapShot');
   });
 });
---
+++ src/fixtures/logged-in-user.js
@@ -0,0 +1,9 @@
+export default {
+  userId: 'testerUserId',
+  username: 'tester',
+  fullName: 'Tester Tester',
+  emailAddress: 'tester@gmail.com',
+  following: ['2'],
+  followers: ['2', '3', '4'],
+  dateCreated: 1622215284350
+};
