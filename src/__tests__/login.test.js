a353b7b9d5a27656eec1d39c3c451ed9ae69e5e4
Igor Shevchenko <bnzone@gmail.com>
2021-06-19 19:30:18 -0700

    add 100% coverage test for Login page

--- src/__tests__/login.test.js
+++ src/__tests__/login.test.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import {
-  render,
-  fireEvent,
-  waitFor,
-  getByTestId
-} from '@testing-library/react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { act } from 'react-dom/test-utils';
 import Login from '../pages/login';
 import FirebaseContext from '../context/firebase';
-import { BrowserRouter as Router } from 'react-router-dom';
+import * as ROUTES from '../constants/routes';
 
 const mockHistoryPush = jest.fn();
-
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useHistory: () => ({
@@ -18,15 +14,21 @@ jest.mock('react-router-dom', () => ({
   })
 }));
 
-describe('<Login />', () => {
-  it('renders the login page with a form submittion and logs the user in', () => {
-    const succededToLogin = jest.fn(() => Promise.resolve('I am signed in!'));
+jest.mock('../services/firebase');
 
+describe('<Login />', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders the login in page with a form submission and logs the user in', async () => {
+    const succeededToLogin = jest.fn(() => Promise.resolve('I am signed in!'));
     const firebase = {
       auth: jest.fn(() => ({
-        signInWithEmailAndPassword: succededToLogin
+        signInWithEmailAndPassword: succeededToLogin
       }))
     };
+
     const { getByTestId, getByPlaceholderText, queryByTestId } = render(
       <Router>
         <FirebaseContext.Provider value={{ firebase }}>
@@ -35,18 +37,71 @@ describe('<Login />', () => {
       </Router>
     );
 
-    expect(document.title).toEqual('Login - SnapShot');
+    await act(async () => {
+      await fireEvent.change(getByPlaceholderText('Email address'), {
+        target: { value: 'tester@gmail.com' }
+      });
+      await fireEvent.change(getByPlaceholderText('Password'), {
+        target: { value: 'tester' }
+      });
+      fireEvent.submit(getByTestId('login'));
 
-    fireEvent.change(getByPlaceholderText('Email address'), {
-      target: {
-        value: 'tester@gmail.com'
-      }
+      expect(document.title).toEqual('Login - SnapShot');
+      expect(succeededToLogin).toHaveBeenCalled();
+      expect(succeededToLogin).toHaveBeenCalledWith(
+        'tester@gmail.com',
+        'tester'
+      );
+
+      await waitFor(() => {
+        expect(mockHistoryPush).toHaveBeenCalledWith(ROUTES.DASHBOARD);
+        expect(getByPlaceholderText('Email address').value).toBe(
+          'tester@gmail.com'
+        );
+        expect(getByPlaceholderText('Password').value).toBe('tester');
+        expect(queryByTestId('error')).toBeFalsy();
+      });
     });
-    fireEvent.change(getByPlaceholderText('Password'), {
-      target: {
-        value: 'tester'
-      }
+  });
+
+  it('renders the login in page with a form submission and fails to log a user in', async () => {
+    const failToLogin = jest.fn(() =>
+      Promise.reject(new Error('Cannot sign in'))
+    );
+    const firebase = {
+      auth: jest.fn(() => ({
+        signInWithEmailAndPassword: failToLogin
+      }))
+    };
+
+    const { getByTestId, getByPlaceholderText, queryByTestId } = render(
+      <Router>
+        <FirebaseContext.Provider value={{ firebase }}>
+          <Login />
+        </FirebaseContext.Provider>
+      </Router>
+    );
+
+    await act(async () => {
+      await fireEvent.change(getByPlaceholderText('Email address'), {
+        target: { value: 'tester@gmail.com' }
+      });
+      await fireEvent.change(getByPlaceholderText('Password'), {
+        target: { value: 'tester' }
+      });
+      fireEvent.submit(getByTestId('login'));
+
+      expect(document.title).toEqual('Login - SnapShot');
+      expect(failToLogin).toHaveBeenCalled();
+      expect(failToLogin).toHaveBeenCalledWith('tester@gmail.com', 'tester');
+      expect(failToLogin).rejects.toThrow('Cannot sign in');
+
+      await waitFor(() => {
+        expect(mockHistoryPush).not.toHaveBeenCalledWith(ROUTES.DASHBOARD);
+        expect(getByPlaceholderText('Email address').value).toBe('');
+        expect(getByPlaceholderText('Password').value).toBe('');
+        expect(queryByTestId('error')).toBeTruthy();
+      });
     });
-    fireEvent.submit(getByTestId('login'));
   });
 });
